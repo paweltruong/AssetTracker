4867b4b7b70a4d473a31cfde96ae1106c2a1ebda
pawciut <paweltruong@o2.pl>
2025-11-06 19:32:23 +0100

    added dummy test so test project is built

---
+++ Stage-02-new-architecture/Src/AssetTracker.Application.Tests/CacheManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using AssetTracker.Application.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace AssetTracker.Application.Tests
+{
+    public class CacheManagerTests
+    {
+        private readonly Mock<ILogger<CacheManager>> _logger;
+
+        public CacheManagerTests()
+        {
+            _logger = new Mock<ILogger<CacheManager>>();
+        }
+
+        ICacheManager BuildSut()
+        {
+            return new CacheManager(_logger.Object);
+        }
+
+
+        [Fact]
+        public void LoadAllMarketplaceDataAsync_WhenHasGames_ReturnsGames()
+        {
+            // Arrange          
+
+            // Act
+
+            // Assert
+          
+        }
+    }
+}
